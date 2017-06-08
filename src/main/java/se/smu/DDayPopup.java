355510e76a8322193abe19b9ddca29862de82800
201511116 <heesu0429@gmail.com>
2017-06-08 23:59:13 +0900

    #724 / To do List 알람 기능 결함 수정

---
+++ src/main/java/se/smu/DDayPopup.java
@@ -0,0 +1,57 @@
+package se.smu;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import java.awt.Font;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class DDayPopup extends JDialog {
+	private final JPanel contentPanel = new JPanel();
+	
+	public DDayPopup(String[] todo){
+		setAlwaysOnTop(true);
+		setBounds(100,100,319,190);
+		setModal(true);
+		setLocationRelativeTo(MainFrame.main_panel);
+		getContentPane().setLayout(new BorderLayout());
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		contentPanel.setLayout(null);
+		
+		JLabel message = new JLabel("");
+		message.setBackground(Color.WHITE);
+		message.setBounds(40,30,190,80);
+		message.setIcon(new ImageIcon(DDayPopup.class.getResource("/icon/alarm.PNG")));
+		contentPanel.add(message);
+		
+		JLabel todo_name = new JLabel(todo[1]);
+		todo_name.setBounds(55,0,152,47);
+		todo_name.setFont(new Font("고딕", Font.PLAIN, 20));
+		contentPanel.add(todo_name);
+		
+		JButton confirm = new JButton("");
+		confirm.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				dispose();
+			}
+		});
+		confirm.setBounds(80,90,150,60);
+		contentPanel.add(confirm);
+		confirm.setActionCommand("확인");
+		getRootPane().setDefaultButton(confirm);
+		confirm.setIcon(new ImageIcon(DDayPopup.class.getResource("/icon/ok_small.PNG")));
+		confirm.setBackground(Color.WHITE);
+		confirm.setBorder(null);
+		
+		
+	}
+}
